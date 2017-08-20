f325790daafb7a67e84ed6308daa6f66e2cdc8bf
mtotodev05 <mtotodev05@gmail.com>
2017-08-20 16:05:24 +0300

    Initial Commit

---
+++ Controllers/admin.php
@@ -0,0 +1,15 @@
+<?php
+class admin{
+
+    public static function main(){
+
+    }
+
+
+}
+
+
+
+
+?>
+
---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>This Is PHP</title>
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<center>
+    <h2> Welcome To PHP Mania</h2>
+</center>
+</body>
+<footer>
+    <center>
+    <h3>Brian Kamau</h3>
+    </center>
+</footer>
+</html>
+<?php
+echo "Hello World";
+
+
+
+?>
